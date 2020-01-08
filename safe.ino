033f334daf42e055b7d572e2ff6a15904eaafb4a
Markus <mj.schwoerer@web.de>
2020-01-08 23:59:11 +0100

    added speaker alarm; added acceleration sensor; added first MQTT functionality;

--- safe.ino
+++ safe.ino
@@ -14,20 +14,46 @@
 #include "Keypad.h"              // keypad library by Mark Stanley, Alexander Brevig
 #include <LiquidCrystal.h>
 #include <LiquidCrystal_I2C.h>   // I2C library by Frank de Brabander
+// acceleration sensor: libraries needed: Adafruit 9DOF; Adafruit ADXL343; Adafruit AHRS; Adafruit BusIO;
+//                                        Adafruit Circuit Playground; Adafruit LSM303 Accel
+#include <Adafruit_LSM303_Accel.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <Adafruit_NeoPixel.h>  // library for LED stripe
 // libraries for ESP OTA update:
 #include <WiFi.h>
 #include "esp_wifi.h"
 #include <ESPmDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
+#include <PubSubClient.h>     // library for MQTT
 
 #define USE_ESP32
+#define WLAN_enable true
 #define CODE_LENGTH 4                    // <-------------------------- safe code LENGTH
 #define USE_I2C_LCD
 #define countdownStart 7
+#define NUMPIXELS      32
+#define MIN_BRIGHT_DEACT 30
+#define MAX_BRIGHT_DEACT 120
+#define MQTTport 1883
+#define thisTopicName "mqtt_5_safe_control"
+#define activateTopicName "mqtt_5_safe_activate"
 
 const char* ssid = "Darknet";
-const char* wlan_password = "2556732986301168";
+const char* wlan_password = "123456789";
+const char* MQTT_BROKER = "........";
+
+bool connectWLAN = WLAN_enable;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+// messages:
+char* puzzleSolved_message = "{\"method\": \"STATUS\", \"state\": \"solved\"}";
 
 // ----------------------------------------------------------------------------------------------
 // keypad:
@@ -76,7 +102,7 @@ const int lockPin = 18;
 int safePassword[CODE_LENGTH] = {4, 2, 4, 2};     // <-------------------------- safe code
 int currentTry[CODE_LENGTH] = {};
 // safe status:
-enum safeStatusEnum {start_state, connectingWLAN_state, noPower_state, locked_state, wrongSafePassword_state, openLock_state, unlocked_state};
+enum safeStatusEnum {start_state, connectingWLAN_state, noPower_state, locked_state, lockedAlarm_state, wrongSafePassword_state, openLock_state, unlocked_state};
 enum safeStatusEnum safeStatus = start_state;
 
 // timer:
@@ -85,11 +111,23 @@ unsigned long startTime, currentTime;
 
 int countdown = countdownStart;
 
+// speaker:
+const int freq = 3000;
+const int freq2 = 4000;
+const int channel = 0;
+const int resolution = 8;
+// Assign a unique ID to this sensor at the same time
+Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(54321);
+
+// LED stripe:
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, 2, NEO_GRB + NEO_KHZ800);
+
 void setup() {
   Serial.begin(115200);
   //Serial.begin(9600);
   pinMode(lockPin, OUTPUT);
   digitalWrite(lockPin, LOW);
+  pinMode(15, OUTPUT);
   safeStatus = start_state;
   printStatus();
   #ifdef USE_I2C_LCD
@@ -99,14 +137,49 @@ void setup() {
     lcd.begin(16, 2);
   #endif
   esp_wifi_set_ps(WIFI_PS_NONE);
-  initOTA();
+  if (connectWLAN) {
+    initOTA();
+    client.setServer(MQTT_BROKER, MQTTport);
+    client.setCallback(callback);
+  }
   initArray();
   safeStatus = noPower_state;
   printStatus();
   countdown = countdownStart;
+  initPWM();
+  //initLEDstripe();
 }
 
+void callback(char* topic, byte* payload, unsigned int length) {
+    Serial.print("Received message [");
+    Serial.print(topic);
+    Serial.print("] ");
+    char msg[length+1];
+    for (int i = 0; i < length; i++) {
+        Serial.print((char)payload[i]);
+        msg[i] = (char)payload[i];
+    }
+    Serial.println();
+ 
+    msg[length] = '\0';
+    Serial.println(msg);
+    safeStatus = locked_state;
+    printStatus();
+}
+
+long piezo_time = 0;
+bool piezo_started = 0;
+int count_num = 0;
+
 void loop() {
+  esp_wifi_set_ps(WIFI_PS_NONE);          // TODO: only every 10s or so ...
+  while (!client.connected()){
+      Serial.println("no connection");
+      client.connect("SafeDevice_M");
+      client.subscribe(activateTopicName);
+    }
+  client.loop();
+  
   if (safeStatus == wrongSafePassword_state) {
     currentTime = millis();
     if (currentTime - startTime >= messageLength) {
@@ -124,6 +197,7 @@ void loop() {
     if (key != NO_KEY) {
       if (key == '*' or key == '#') {
         safeStatus = unlocked_state;
+        client.publish(thisTopicName, puzzleSolved_message);
       }
     }
     initArray();
@@ -136,10 +210,12 @@ void loop() {
     }
   }
   else if (safeStatus == noPower_state) {
-    char key = keypad.getKey();       // TODO: remove and implement MQTT here
-    if (key != NO_KEY) {
-      if (key == '#' or key == '*') {
-        lock();
+    if (!WLAN_enable) {
+      char key = keypad.getKey();       // TODO: remove and implement MQTT here
+      if (key != NO_KEY) {
+        if (key == '#' or key == '*') {
+          lock();
+        }
       }
     }
   }
@@ -155,10 +231,13 @@ void loop() {
   else {
     digitalWrite(lockPin, LOW);
   }
-  if (WiFi.waitForConnectResult() != WL_CONNECTED) {    // TODO: better solution? problem: ESP loses the current state if the connection is lost!
-    initOTA();
+  if (connectWLAN) {
+    if (WiFi.waitForConnectResult() != WL_CONNECTED) {    // TODO: better solution? problem: ESP loses the current state if the connection is lost!
+      initOTA();
+    }
+    ArduinoOTA.handle();
   }
-  ArduinoOTA.handle();
+  sensorEvent();
 }
 
 void reset() {
@@ -242,6 +321,9 @@ void printStatus() {
     case locked_state:
       lcd.print("* SAFE LOCKED! *");
       break;
+    case lockedAlarm_state:
+      lcd.print("* DONT DO THIS *");
+      break;
     case wrongSafePassword_state:
       lcd.print("** CODE WRONG **");
       break;
@@ -312,15 +394,132 @@ void printCountdown() {
   }
 }
 
+void initPWM() {
+  // init pwm
+  ledcSetup(channel, freq, resolution);
+  ledcAttachPin(4, channel);
+  ledcWrite(channel, 0);
+  /* Initialise the sensor */
+  if (!accel.begin()) {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+    while (1)
+      ;
+  }
+
+
+  accel.setRange(LSM303_RANGE_2G);
+  Serial.print("Range set to: ");
+  lsm303_accel_range_t new_range = accel.getRange();
+  switch (new_range) {
+  case LSM303_RANGE_2G:
+    Serial.println("+- 2G");
+    break;
+  case LSM303_RANGE_4G:
+    Serial.println("+- 4G");
+    break;
+  case LSM303_RANGE_8G:
+    Serial.println("+- 8G");
+    break;
+  case LSM303_RANGE_16G:
+    Serial.println("+- 16G");
+    break;
+  }
+
+  accel.setMode(LSM303_MODE_NORMAL);
+  Serial.print("Mode set to: ");
+  lsm303_accel_mode_t new_mode = accel.getMode();
+  switch (new_mode) {
+  case LSM303_MODE_NORMAL:
+    Serial.println("Normal");
+    break;
+  case LSM303_MODE_LOW_POWER:
+    Serial.println("Low Power");
+    break;
+  case LSM303_MODE_HIGH_RESOLUTION:
+    Serial.println("High Resolution");
+    break;
+  }
+}
+
+void sensorEvent() {
+  /* Get a new sensor event */
+  sensors_event_t event;
+  accel.getEvent(&event);
+  double vec = sqrt((event.acceleration.x)*(event.acceleration.x) + (event.acceleration.y)*(event.acceleration.y) + (event.acceleration.z)*(event.acceleration.z));
+  Serial.println(vec);
+  //if(vec > 20){
+  if((safeStatus == locked_state) and (vec > 20)){
+     // piezo_started = 1;
+     safeStatus = lockedAlarm_state;
+     printStatus();
+     piezo_time = millis();
+     ledcWrite(channel, 125);
+     ledcWriteTone(channel, freq);
+  }
+  //if(piezo_started){
+  if(safeStatus == lockedAlarm_state){
+    long time_diff = floor((millis() - piezo_time)/100)*100;
+    if(time_diff % 1000 == 0) {
+      ledcWriteTone(channel, freq);
+    } else if(time_diff % 500 == 0){
+      ledcWriteTone(channel, freq2);
+    }
+    if(time_diff > 3000){
+      ledcWrite(channel, 0);
+      //piezo_started = 0;
+      safeStatus = locked_state;
+      printStatus();
+    }
+  }
+}
+
+void initLEDstripe() {
+  pixels.begin();
+  pixels.clear();
+    for(int i=0;i<NUMPIXELS;i++){
+    pixels.setPixelColor(i, pixels.Color(255, 200, 50));
+    pixels.show(); // This sends the updated pixel color to the hardware.
+  }
+  //delay(100);         // TODO: remove?
+}
+
+void setLEDstripe() {
+ for(int i=0;i<NUMPIXELS;i++){
+    pixels.setPixelColor(i, pixels.Color(255, 200, 50));
+    pixels.show(); // This sends the updated pixel color to the hardware.
+  }
+ for(int i = MIN_BRIGHT_DEACT; i < MAX_BRIGHT_DEACT;i+= 2){
+  pixels.setBrightness(i);
+ pixels.show(); // This sends the updated pixel color to the hardware.
+  delay(100);
+ }
+ for(int i = MAX_BRIGHT_DEACT; i > MIN_BRIGHT_DEACT;i-= 2){
+  pixels.setBrightness(i);
+  pixels.show(); // This sends the updated pixel color to the hardware.
+  //delay(100);       // TODO: remove?
+ }
+  //pixels.clear();
+}
+
 void initOTA() {
   safeStatus = connectingWLAN_state;
   printStatus();
   Serial.println("Booting");
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, wlan_password);
+  int x = millis();
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    delay(5000);
-    ESP.restart();
+    char key = keypad.getKey();
+    if (key != NO_KEY) {
+      if (key == '*' or key == '#') {
+        connectWLAN = false;
+        return;
+      }
+    }
+    if (x + 5000 < millis()) {
+      ESP.restart();
+    }
   }
 
   // Port defaults to 3232
