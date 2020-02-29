d800ded87d59bbdd5493fe20f005a56d4572ee2d
FlorianBre <36412411+FlorianBre@users.noreply.github.com>
2020-02-29 10:02:01 +0100

    Added some comments

--- safe_activation/safe_activation.ino
+++ safe_activation/safe_activation.ino
@@ -7,6 +7,7 @@
 #include <Preferences.h>
 #include <ArduinoJson.h>
 
+// Placeholder for ssid and password read from ram
 const char* key_ssid = "ssid";
 const char* key_pwd = "pass";
 
@@ -17,6 +18,7 @@ Preferences preferences;
 // Add your MQTT Broker IP address, example:
 const char* mqtt_server = "10.0.0.2";
 
+// Status messages
 char* puzzleSolved_message = "{\"method\": \"status\", \"state\": \"solved\"}";
 char* puzzleActive_message = "{\"method\": \"status\", \"state\": \"active\"}";
 char* puzzleInactive_message = "{\"method\": \"status\", \"state\": \"inactive\"}";
@@ -85,7 +87,9 @@ void setup() {
   s7_val = 0;
   s8_val = 0;
   adc_correct_value = 0;
+  // Init ADC GPIO
   pinMode(A0,INPUT);
+  // Init GPIO for checking the switches
   pinMode(S1,INPUT_PULLUP);
   pinMode(S2,INPUT_PULLUP);
   pinMode(S3,INPUT_PULLUP);
@@ -104,9 +108,12 @@ void setup() {
 }
 
 void loop() {
-  Serial.print("Current State:"); Serial.println(puzzle_status); 
+  // Debug output current state
+  //Serial.print("Current State:"); Serial.println(puzzle_status); 
+  // Always connect to the mqqt server
   connect_mqqt();
   if(puzzle_status == STATE_ACTIVE){  
+    // Check wether puzzle are solved
     voltage_status = check_voltage();
     switch_status = check_switches();
     // Publish solved Message once if both parts are solved 
@@ -121,27 +128,92 @@ void loop() {
   ArduinoOTA.handle();
 }
 
+// Function returning 1 when switches are in the correct position
+unsigned int check_switches(){
+    s1_val = digitalRead(S1);
+    s2_val = digitalRead(S2);
+    s3_val = digitalRead(S3);
+    s4_val = digitalRead(S4);
+    s5_val = digitalRead(S5);
+    s6_val = digitalRead(S6);
+    s7_val = digitalRead(S7);
+    s8_val = digitalRead(S8);
+  /*
+  // Debug output
+  Serial.print("S1: ");  Serial.println(s1_val);
+  Serial.print("S2: ");  Serial.println(s2_val);
+  Serial.print("S3: ");  Serial.println(s3_val);
+  Serial.print("S4: ");  Serial.println(s4_val);
+  Serial.print("S5: ");  Serial.println(s5_val);
+  Serial.print("S6: ");  Serial.println(s6_val);
+  Serial.print("S7: ");  Serial.println(s7_val);
+  Serial.print("S8: ");  Serial.println(s8_val);
+  */
+  // If switches are in the correct position set status LED to High
+  if(!s2_val && !s3_val && !s6_val && !s8_val && s1_val && s4_val && s5_val && s7_val){
+    Serial.println("Puzzle Switches Solved");
+    delay(200);
+    digitalWrite(LED_S_SOLVED,HIGH);
+    return SWITCH_SOLVED;
+  }
+  else{
+    delay(200);
+    digitalWrite(LED_S_SOLVED,LOW);
+    return SWITCH_NOT_SOLVED;
+  }
+}
+
+// returns 1 when sensing the correct voltage
+unsigned int check_voltage(){
+  adc_val = analogRead(A0);
+  // Debug output
+  //Serial.print("ADC VAL: "); Serial.println(adc_val);
+  // Turn on led and return 1 if measured voltage is between Thresholds
+  if((adc_val < HIGHER_THRESHOLD) && (adc_val > LOWER_THRESHOLD)){
+    adc_correct_value = adc_correct_value + 1;
+    // Only return solved when there are a least 5 consecutive measurements in the right voltage range
+    if(adc_correct_value > AMOUNT_CORRECT_MEAS){
+        digitalWrite(LED_V_SOLVED,HIGH);
+        Serial.println("Puzzle Voltage Solved");
+        adc_correct_value = 0;
+        return VOLT_SOLVED;
+    }
+    else{
+      return VOLT_NOT_SOLVED;
+    }
+  }
+  else{
+    digitalWrite(LED_V_SOLVED,LOW);
+    adc_correct_value = 0;
+    return VOLT_NOT_SOLVED;
+  }
+  return VOLT_NOT_SOLVED;
+}
+
+
 void setup_wifi() {
   delay(10);
-  // We start by connecting to a WiFi network
+  // Connect to selected WiFi Network
   Serial.println();
   Serial.print("Connecting");
   int timeout_start = millis();
   char ssid[30];
   char wlan_password[30];
   preferences.begin("wifi", false); 
+  // Get password and ssid from ram
   preferences.getString(key_pwd, wlan_password, 30);
   preferences.getString(key_ssid, ssid, 30);
   preferences.end();
   WiFi.begin(ssid, wlan_password);
-
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
+      // Restart ESP when no connection to the Network is established
      if (timeout_start + WIFI_TIMEOUT_MS < millis()) {
       ESP.restart();
     }
   }
+  // Turn LED off to signalise that the device is connected to the Network
   digitalWrite(LED_S_SOLVED,LOW);
   digitalWrite(LED_V_SOLVED, LOW);
   Serial.println("");
@@ -180,67 +252,7 @@ void initOTA() {
     ArduinoOTA.begin();
 }
 
-// returns one when sensing the correct voltage
-unsigned int check_voltage(){
-  adc_val = analogRead(A0);
-  Serial.print("ADC VAL: "); Serial.println(adc_val);
-  // Turn on led and return 1 if measured voltage is between Thresholds
-  if((adc_val < HIGHER_THRESHOLD) && (adc_val > LOWER_THRESHOLD)){
-    adc_correct_value = adc_correct_value + 1;
-    // Low Tech "high pass" filter
-    if(adc_correct_value > AMOUNT_CORRECT_MEAS){
-        digitalWrite(LED_V_SOLVED,HIGH);
-        Serial.println("Puzzle Voltage Solved");
-        adc_correct_value = 0;
-        return VOLT_SOLVED;
-    }
-    else{
-      return VOLT_NOT_SOLVED;
-    }
-  }
-  else{
-    digitalWrite(LED_V_SOLVED,LOW);
-    adc_correct_value = 0;
-    return VOLT_NOT_SOLVED;
-  }
-  return VOLT_NOT_SOLVED;
-}
-
-// Callback by switch change
-unsigned int check_switches(){
-    s1_val = digitalRead(S1);
-    s2_val = digitalRead(S2);
-    s3_val = digitalRead(S3);
-    s4_val = digitalRead(S4);
-    s5_val = digitalRead(S5);
-    s6_val = digitalRead(S6);
-    s7_val = digitalRead(S7);
-    s8_val = digitalRead(S8);
-  // Only for Debugging
-  /*
-  Serial.print("S1: ");  Serial.println(s1_val);
-  Serial.print("S2: ");  Serial.println(s2_val);
-  Serial.print("S3: ");  Serial.println(s3_val);
-  Serial.print("S4: ");  Serial.println(s4_val);
-  Serial.print("S5: ");  Serial.println(s5_val);
-  Serial.print("S6: ");  Serial.println(s6_val);
-  Serial.print("S7: ");  Serial.println(s7_val);
-  Serial.print("S8: ");  Serial.println(s8_val);
-  */
-  
-  if(!s2_val && !s3_val && !s6_val && !s8_val && s1_val && s4_val && s5_val && s7_val){
-    Serial.println("Puzzle Switches Solved");
-    delay(200);
-    digitalWrite(LED_S_SOLVED,HIGH);
-    return SWITCH_SOLVED;
-  }
-  else{
-    delay(200);
-    digitalWrite(LED_S_SOLVED,LOW);
-    return SWITCH_NOT_SOLVED;
-  }
-}
-
+// Mqqt callback function
 void callback(char* topic, byte* payload, unsigned int length) {
     Serial.print("Received message [");
     Serial.print(topic);
@@ -254,7 +266,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
  
     msg[length] = '\0';
     Serial.println(msg);
-    
+    // Deserialzie messafe
     deserializeJson(mqtt_decoder, msg);
     const char* method_msg = mqtt_decoder["method"];
     const char* state_msg = mqtt_decoder["state"];
@@ -263,16 +275,19 @@ void callback(char* topic, byte* payload, unsigned int length) {
     Serial.println(state_msg);
     Serial.println(strcmp(method_msg, "trigger") == 0);
     Serial.println(strcmp(state_msg, "on") == 0);
+    // If trigger on message was sended
     if(strcmp(topic, "5/safe/activate") == 0 && strcmp(method_msg, "trigger") == 0 && strcmp(state_msg, "on") == 0){
       puzzle_status = STATE_ACTIVE;
       client.publish("5/safe/activate", puzzleActive_message, true);
     }
+   // If trigger off message was sended
     if(strcmp(topic, "5/safe/activate") == 0 && strcmp(method_msg, "trigger") == 0 && strcmp(state_msg, "off") == 0){
       puzzle_status = STATE_INACTIVE;
       client.publish("5/safe/activate", puzzleInactive_message, true);
     }  
 }
 
+// Function connecting to the mqqt server and suscribing the topic
 void connect_mqqt(){
 	client.loop();
   while (!client.connected()){
